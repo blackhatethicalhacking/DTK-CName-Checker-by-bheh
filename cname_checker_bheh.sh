6f216f159b76411fcf147d8c26ed2bf7b239763f
root <root@blackhatethicalhacking>
2020-11-22 22:16:26 +0200

    added lolcat

--- cname_checker_bheh.sh
+++ cname_checker_bheh.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-echo "Hello from SaintDruG! Here ya go:"
+echo "Hello from SaintDruG! Checking for DTK!:" | lolcat
 domain=$1
 while read sub; do 
 # Here, we will check after it resolves CNAME using techniques like Grep, AWK to tidy things, NF is Number of Fields you can replace with $6 ;)
    host -t CNAME "$sub.$domain" | grep 'alias for' | awk '{print $NF}' |
    while read cname; do
    if ! host "$cname" &> /dev/null; then
-    echo "$cname does not resolve baby to this ($sub.$domain) Check XYZ for Possible DTK"
+    echo "$cname does not resolve baby to this ($sub.$domain) Check XYZ for Possible DTK" | lolcat
 	fi
     done
 done
